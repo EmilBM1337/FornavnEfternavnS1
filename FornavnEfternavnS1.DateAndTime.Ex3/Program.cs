85a65ad399d5d015f9c7f16a9ba4659b6d2cf2a2
emil41m5 <emil41m5@CV-BB-5340>
2021-09-14 09:55:10 +0200

    .

--- FornavnEfternavnS1.DateAndTime.Ex3/Program.cs
+++ FornavnEfternavnS1.DateAndTime.Ex3/Program.cs
@@ -6,7 +6,32 @@ namespace FornavnEfternavnS1.DateAndTime.Ex3
     {
         static void Main(string[] args)
         {
-            
+            //Opgave 1
+
+            Console.WriteLine("Skriv et årstal, og jeg fortæller hvornår det næste skudår er.");
+
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (DateTime.MinValue.Year >= userInput || userInput >= DateTime.MaxValue.Year)
+            {
+                Console.WriteLine("Forkert årstal!");
+            }
+            else
+            {
+                if (DateTime.IsLeapYear(userInput))
+                {
+                    Console.WriteLine("Det er skudår!");
+                }
+                else
+                {
+                    while (!DateTime.IsLeapYear(userInput))
+                    {
+                        Console.WriteLine(userInput + " Det er ikke skudår!");
+                        userInput++;
+                    }
+                    Console.WriteLine(userInput + " Det er skudår!");
+                }
+            }
         }
     }
 }
